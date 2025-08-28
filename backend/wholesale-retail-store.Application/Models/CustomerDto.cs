0ed65ada2792f022b49005e31c954f66ae184f6b
Jamila <jamila.moe95@gmail.com>
2025-08-28 16:03:40 +0200

    removed bff for simplicity and time constraints

--- backend/wholesale-retail-store.Application/Models/CustomerDto.cs
+++ backend/wholesale-retail-store.Application/Models/CustomerDto.cs
@@ -4,5 +4,7 @@ public class CustomerDto
 {
     public Guid Id { get; set; }
     public string Data { get; set; }
+    public string Name { get; set; }
+    public bool IsRetailer { get; set; }
     public DateTime Created { get; set; }
 }
--- backend/wholesale-retail-store.Infrastructure/Repositories/CustomerRepository.cs
+++ backend/wholesale-retail-store.Infrastructure/Repositories/CustomerRepository.cs
@@ -1,13 +1,20 @@
 using wholesale_retail_store.Domain.Entities;
 using wholesale_retail_store.Domain.Interface;
+using wholesale_retail_store.Infrastructure.Persistence;
 
 namespace wholesale_retail_store.Infrastructure.Repositories;
 
 public class CustomerRepository : ICustomerRepository
 {
-    public Task CreateCustomerAsync()
+    private readonly AppDbContext _context;
+    public CustomerRepository(AppDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+    public async Task CreateCustomerAsync(Customer customer)
+    {
+        await _context.Customers.AddAsync(customer);
+        await _context.SaveChangesAsync();
     }
 
     public Task<Customer> GetCustomerByIdAsync(Guid id)
